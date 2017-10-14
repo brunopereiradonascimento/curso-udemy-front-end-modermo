1f07f5a60370ade6f19bf846320853fd89bd719e
Bruno Pereira do Nascimento <brunopereiradonascimento@gmail.com>
2017-10-14 19:13:09 -0300

    finalização do menu - inclusão de arquivos

---
+++ src/javascript/menu.js
@@ -0,0 +1,40 @@
+;(function(){
+    'use strict';
+    
+    var btn = document.querySelector('.header-nav__hamburgger');
+    var html = document.querySelector('html');
+    var menu = document.querySelector('#mainMenu');
+    var classMenu = 'menu-opened';
+    var menuOpened = false;
+    
+    html.addEventListener('click', function(e){
+       if(e.target === html && menuOpened) {
+           closeMenu();
+       }
+    });
+    btn.addEventListener('click', toggleMenu);
+    
+    function toggleMenu(e){
+        if(menuOpened){
+            closeMenu();
+        } else {
+            openMenu();
+        }
+    }
+    
+    function closeMenu(){
+        menuOpened = false;
+        html.classList.remove('menu-opened'); 
+        btn.blur();
+        
+        menu.setAttribute('aria-expanded', false);
+        btn.setAttribute('aria-expanded', false);        
+    }
+    
+    function openMenu(){
+        menuOpened = true;
+        html.classList.add('menu-opened');
+        menu.setAttribute('aria-expanded', true);
+        btn.setAttribute('aria-expanded', true);
+    }    
+}())
