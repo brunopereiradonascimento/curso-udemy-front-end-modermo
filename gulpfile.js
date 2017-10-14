c5f8b0d4db4615c10ef4bd9579e15b4a396b5115
Bruno Pereira do Nascimento <brunopereiradonascimento@gmail.com>
2017-10-13 21:58:52 -0300

    Finalização do header animado

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass');
+//wait = require('gulp-wait');
 
 gulp.task('sass', function() {
     gulp.src('./src/sass/**/*.scss')
         .pipe(sass())
+        //.pipe(wait(1500))
         .pipe(gulp.dest('./src/css'));
 });
 
